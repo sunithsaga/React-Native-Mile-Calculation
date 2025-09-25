143dd3a23c651a6d94bca2930fd012e613f170d7
Sunith <sunith.sudakaran@gmail.com>
2025-09-25 17:17:09 +0100

    fix (UI): seprated header component + UI

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,45 +1,39 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Appbar } from "react-native-paper";
+import { View } from 'react-native';
 
-const styles = StyleSheet.create({
-  headerContainer: {
-    backgroundColor: "#fff",
-    elevation: 4, // shadow for Android
-    shadowColor: "#000", // shadow for iOS
-    shadowOpacity: 0.1,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 3,
-  },
-  header: {
-    backgroundColor: "#6200ee", // theme color
-  },
-  headerTitle: {
-    color: "white",
-    fontWeight: "bold",
-  },
-  headingSection: {
-    borderBottomWidth: 2,
-    borderBottomColor: "#ccc",
-    paddingVertical: 12,
-    paddingHorizontal: 16,
-    backgroundColor: "#f9f9f9",
-  },
-  headingText: {
-    fontWeight: "600",
-    color: "#333",
-  },
-});
+import { HeaderComponent } from '@/components/header';
+import { Button, TextInput } from 'react-native-paper';
 
 const HomeComponent = () => {
+    const [text, setText] = React.useState("");
   return (
-        <View style={styles.headerContainer}>
-      <Appbar.Header style={styles.header} mode='center-aligned'>
-        <Appbar.Content title="Mile Calculator" titleStyle={styles.headerTitle} />
-      </Appbar.Header>
 
+    <View >
+<HeaderComponent/>
+
+  <view style={{padding:40, marginTop:30, width:'80%'}}>
+   <TextInput
+      label="Enter start location"
+      value={text}
+      mode='outlined'
+     
+      onChangeText={text => setText(text)}
+    />
+    <TextInput
+    style={{marginTop:30}}
+      label="Enter end location"
+      value={text}
+      mode='outlined'
+      onChangeText={text => setText(text)}
+    />
+
+    <Button  mode="contained" onPress={() => console.log('Pressed')}>
+   Calculate Miles
+  </Button>
+  </view>
 
     </View>
+
   );
 };
 
